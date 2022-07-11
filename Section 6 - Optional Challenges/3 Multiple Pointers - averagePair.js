67ca0356f628bc549e94ce37b39c304b60f7c6bf
Pablo <tackpablo@gmail.com>
2022-07-11 07:46:26 -0700

    add multiple pointers - averagePair solution

---
+++ Section 6 - Optional Challenges/3 Multiple Pointers - averagePair.js
@@ -0,0 +1,23 @@
+function averagePair(arr, num) {
+    // initialize both pointers (one at start and other at end of array)
+    let start = 0;
+    let end = arr.length - 1;
+
+    // while start is less than end
+    while (start < end) {
+        // calculate averge
+        let avg = (arr[start] + arr[end]) / 2;
+
+        //if average is equal to num, return true
+        if (avg === num) return true;
+        // else if average is less than num, increase start
+        else if (avg < num) start++;
+        // else average is greater than num, reduce end
+        else end--;
+    }
+
+    // otherwise return false
+    return false;
+}
+
+// solution is possible due to array being sorted
